7a7a7241dfa3ec5b0ee9a5905fc034c6524906ed
Mark Lavin <mlavin@caktusgroup.com>
2011-09-28 16:23:10 -0400

    Testing email backend.

---
+++ runtests.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import os
+import sys
+
+from django.conf import settings
+
+
+if not settings.configured:
+    settings.configure(
+        DATABASES={
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': 'test.db',
+            }
+        },
+        INSTALLED_APPS=(
+            'django.contrib.auth',
+            'django.contrib.contenttypes',
+            'django.contrib.sessions',
+            'django.contrib.sites',
+            'django.contrib.messages',
+            'bandit',
+        ),
+        SITE_ID=1,
+        ADMINS=(('Admin', 'admin@example.com'), ),
+        BANDIT_EMAIL='bandit@example.com',
+    )
+
+
+from django.test.utils import get_runner
+
+
+def runtests(*test_args):
+    if not test_args:
+        test_args = ['bandit']
+    parent = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", )
+    sys.path.insert(0, parent)
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner(verbosity=1, interactive=True, failfast=False)
+    failures = test_runner.run_tests(test_args)
+    sys.exit(failures)
+
+
+if __name__ == '__main__':
+    runtests(*sys.argv[1:])
+
