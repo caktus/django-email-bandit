3e7fc00935f33b561f1d2fcde2b179511a9d007d
Tobias McNulty <tobias@caktusgroup.com>
2011-11-18 14:26:34 -0500

    convert HijackBackend into a Mixin so it can be used with other EmailBackends

--- bandit/backends.py
+++ bandit/backends.py
@@ -5,17 +5,17 @@ from django.core.mail.backends.smtp import EmailBackend as SMTPBackend
 from django.template.loader import render_to_string
 
 
-class HijackBackend(SMTPBackend):
+class HijackBackendMixin(object):
     """
-    This backend intercepts outgoing messages drops them to a single email
-    address.
+    This backend mixin intercepts outgoing messages drops them to a single
+    email address.
     """
 
     def send_messages(self, email_messages):
         admins = getattr(settings, 'ADMINS', ())
-        sever_email = getattr(settings, 'SERVER_EMAIL', 'root@localhost')
-        bandit_email = getattr(settings, 'BANDIT_EMAIL', sever_email)
-        approved_emails = set([sever_email, bandit_email, ] + [email for name, email in admins])
+        server_email = getattr(settings, 'SERVER_EMAIL', 'root@localhost')
+        bandit_email = getattr(settings, 'BANDIT_EMAIL', server_email)
+        approved_emails = set([server_email, bandit_email, ] + [email for name, email in admins])
         for message in email_messages:
             all_approved = reduce(and_, map(lambda e: e in approved_emails, message.to))
             if not all_approved:            
@@ -24,4 +24,12 @@ class HijackBackend(SMTPBackend):
                 header = render_to_string("bandit/hijacked-email-header.txt", context)
                 message.body = header + message.body
                 message.to = [bandit_email, ]
-        return super(HijackBackend, self).send_messages(email_messages)
+        return super(HijackBackendMixin, self).send_messages(email_messages)
+
+
+class HijackBackend(HijackBackendMixin, SMTPBackend):
+    """
+    This backend intercepts outgoing messages drops them to a single email
+    address.
+    """
+    pass
